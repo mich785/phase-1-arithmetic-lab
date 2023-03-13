d5879853d8c911f90480c97b9cd38c5a22422b5d
michelle nderi <nderimichelle@gmail.com>
2023-03-13 23:51:04 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+const num1 = 31;
+const num2 = 2;
 
-// Write your code here
+ let multiply= num1 * num2 ;
+
+let random= Math.floor(Math.random() * 10) + 1;
+
+const num3= 32;
+const num4 =8;
+
+let mod= num3 / num4;
+
+ let max =Math.max(16 , 17 ,18 ,19, 20);
+
+
+
